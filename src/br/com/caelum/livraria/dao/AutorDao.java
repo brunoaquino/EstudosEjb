e7b2f57a349c8ca7cd8822782ddc3417af9396fa
Bruno Carvalho de Aquino <bruno-smv2@hotmail.com>
2016-08-03 10:32:54 -0300

    Criando projeto

---
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -0,0 +1,30 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void salva(Autor autor) {
+		manager.persist(autor);
+	}
+
+	public List<Autor> todosAutores() {
+		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
+	}
+
+	public Autor buscaPelaId(Integer autorId) {
+		Autor autor = this.manager.find(Autor.class, autorId);
+		return autor;
+	}
+
+}
